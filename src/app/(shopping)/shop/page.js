b8536ae12ac54537d4c869b27560f6860b6e6bff
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2024-09-06 17:37:51 -0500

    fixing deploy

--- src/app/(shopping)/shop/page.js
+++ src/app/(shopping)/shop/page.js
@@ -1,7 +1,7 @@
 import InventoryList from "@/components/inventory/InventoryList";
 
 export default async function ShopPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/inventory`, { cache: 'no-store' });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/data/sample_inventory_response.json`, { cache: 'no-store' });
   const data = await res.json();
 
   return <InventoryList items={data} />;
