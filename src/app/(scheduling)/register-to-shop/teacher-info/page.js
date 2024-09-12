3c3418ad611e6bfbd1cc5d5435420ba639833516
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2024-09-12 15:39:05 -0500

    combined confirmed and user registration view

--- src/app/(scheduling)/register-to-shop/teacher-info/page.js
+++ src/app/(scheduling)/register-to-shop/teacher-info/page.js
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { CheckIcon } from "@heroicons/react/20/solid";
 
 const initialState = {
   email: "",
@@ -21,6 +22,15 @@ const sampleSchoolData = {
   time: "12:00 PM CST",
 };
 
+
+const constantData = {
+  email: "email@pencilbox.com",
+  phoneNumber: "###-###-####",
+  questionOrConcern: "If you have any questions or concerns please contact Pencil Box:",
+  reminder: "You will receive an email 24 hours before your scheduled time for self-check in to start your shopping process.",
+  thanks: "Thanks for signing up.",
+}
+
 export default function TeacherInfoPage({ params, searchParams }) {
   const [email, setEmail] = useState(searchParams.email);
   const [firstName, setFirstName] = useState(initialState.firstName);
@@ -28,6 +38,7 @@ export default function TeacherInfoPage({ params, searchParams }) {
   const [loading, setLoading] = useState(true);
   const [school, setSchool] = useState(searchParams.school_id);
   const [selectedShop, setSelectedShop] = useState({});
+  const [confirmed, setConfirmed] = useState(false);
 
   const router = useRouter();
 
@@ -46,6 +57,9 @@ export default function TeacherInfoPage({ params, searchParams }) {
     console.log("Email", email);
     console.log("School", school);
     console.log("YOU SUBMITTED, YA FILTHY ANIMAL! We need an API");
+
+    // ON SUCCESS, ROUTE TO CONFIRMATION
+    setConfirmed(true);
 
     resetForm();
   };
@@ -72,92 +86,130 @@ export default function TeacherInfoPage({ params, searchParams }) {
   return (
     <div className="flex flex-col items-center justify-center mt-[50px] relative z-[1000]">
       <div className="p-4 rounded-lg shadow-md w-full max-w-md bg-gray-50">
-      <h2 className="font-bold text-3xl mb-5">Confirm Time</h2>
+        {confirmed ? (
+          <>
+            <div className="flex gap-5 items-center mb-10">
+              <p className="font-bold text-3xl">Time Confirmed</p>
+              <CheckIcon className="bg-green-700 h-8 p-1 rounded-full text-white/90" />
+            </div>
+            <div className="p-6 rounded-md mb-6 bg-black">
+              <p className="font-bold text-2xl font-bold text-white">
+                {selectedShop.date}
+              </p>
+              <p className="text-lg text-white">Time: {selectedShop.time}</p>
+              <p className="text-lg text-white">
+                Location: {selectedShop?.location?.loc}
+              </p>
+            </div>
+            <div>
+              <p className="font-semibold text-xl pb-5">{constantData.thanks}</p>
+              <p className="font-medium text-lg pb-5">{constantData.reminder}</p>
+              <p className="text-black/50 font-light">
+                {constantData.questionOrConcern} {constantData.email} or{" "}
+                {constantData.phoneNumber}
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2 className="font-bold text-3xl mb-5">Confirm Time</h2>
 
-      <div className="pt-6 rounded-md mb-6">
-          <p className="text-lg font-bold text-gray-900">{selectedShop.date}</p>
-          <p className="text-lg text-gray-700">Time: {selectedShop.time}</p>
-          <p className="text-lg text-gray-700">
-            Location: {selectedShop?.location?.loc}
-          </p>
-          <p
-            className={`text-lg ${
-              availability ? "text-green-600 " : "text-red-600"
-            } font-semibold`}
-          >
-            Available Spots: {availability}
-          </p>
-        </div>
+            <div className="pt-6 rounded-md mb-6">
+              <p className="text-lg font-bold text-gray-900">
+                {selectedShop.date}
+              </p>
+              <p className="text-lg text-gray-700">Time: {selectedShop.time}</p>
+              <p className="text-lg text-gray-700">
+                Location: {selectedShop?.location?.loc}
+              </p>
+              <p
+                className={`text-lg ${
+                  availability ? "text-green-600 " : "text-red-600"
+                } font-semibold`}
+              >
+                Available Spots: {availability}
+              </p>
+            </div>
+          </>
+        )}
 
-      <form className="space-y-4" onSubmit={handleSubmit}>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            First Name
-          </label>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            placeholder="First Name"
-            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            Last Name
-          </label>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            placeholder="Last Name"
-            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            Email Address
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="example@email.com"
-            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-gray-700">
-            School Name
-          </label>
-          <select
-            value={school}
-            onChange={(e) => setSchool(e.target.value)}
-            className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            <option value="" disabled>Select School</option>
-            <option value="1">School 1</option>
-            <option value="2">School 2</option>
-            {/* TODO: Connect to API Add more schools as needed */}
-          </select>
-        </div>
-        <div className="flex flex-col">
-          <button
-            type="submit"
-            className="flex-1 bg-blue-900 text-white px-4 py-4 rounded-md hover:bg-blue-600 mb-4 transition-colors"
-          >
-            Confirm
-          </button>
+        {!confirmed ? (
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                First Name
+              </label>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                placeholder="First Name"
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Last Name
+              </label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder="Last Name"
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Email Address
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="example@email.com"
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                School Name
+              </label>
+              <select
+                value={school}
+                onChange={(e) => setSchool(e.target.value)}
+                className="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                required
+              >
+                <option value="" disabled>
+                  Select School
+                </option>
+                <option value="1">School 1</option>
+                <option value="2">School 2</option>
+                {/* TODO: Connect to API Add more schools as needed */}
+              </select>
+            </div>
+            <div className="flex flex-col">
+              <button
+                type="submit"
+                className="flex-1 bg-blue-900 text-white px-4 py-4 rounded-md hover:bg-blue-600 mb-4 transition-colors"
+              >
+                Confirm
+              </button>
 
-          <button
-            type="button"
-            onClick={() => router.push("/select-time")}
-            className="w-full text-red-600 px-4 py-4 rounded-md border-red-600 border hover:bg-red-800 hover:text-white transition-colors"
-          >
-            Select a Different Time
-          </button>
-        </div>
-      </form>
+              <button
+                type="button"
+                onClick={() => router.push("/select-time")}
+                className="w-full text-red-600 px-4 py-4 rounded-md border-red-600 border hover:bg-red-800 hover:text-white transition-colors"
+              >
+                Select a Different Time
+              </button>
+            </div>
+          </form>
+        ) : null}
       </div>
     </div>
   );
