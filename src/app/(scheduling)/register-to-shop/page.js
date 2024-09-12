bdbcf26caeb129e4b1ebbfc1ae54e47d5b32aa35
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2024-09-12 14:31:22 -0500

    modify avail and reg page and connect schedule to router

--- src/app/(scheduling)/register-to-shop/page.js
+++ src/app/(scheduling)/register-to-shop/page.js
@@ -1,8 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from 'next/navigation'
-
+import { useRouter } from "next/navigation";
 
 const intialState = {
   email: "",
@@ -10,22 +9,26 @@ const intialState = {
 };
 
 const schedule_item_sample = {
+  id: 1,
   date: "January 21, 2025",
+  location: {
+    id: 1,
+    availability: 10,
+    loc: "Nashville Pencil Box",
+  },
   time: "12:00 PM CST",
-  location: "Nashville Pencil Box",
-  available_spots: 0,
 };
 
-// https://example.com/register-to-shop?schedule_item_id={ID}
 export default function ConfirmTime({ params, searchParams }) {
   const [email, setEmail] = useState(intialState.email);
   const [school, setSchool] = useState(intialState.school);
-  const [scheduleItem, setScheduleItem] = useState(schedule_item_sample);
+  const [scheduleItem, setScheduleItem] = useState({});
 
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     // MAKE CALL TO API USING SEARCHPARAMS ID :
+    setScheduleItem(schedule_item_sample);
   }, [params, searchParams]);
 
   const resetForm = () => {
@@ -43,6 +46,8 @@ export default function ConfirmTime({ params, searchParams }) {
     resetForm();
   };
 
+  const availability = scheduleItem?.location?.availability;
+
   return (
     <div className="flex flex-col items-center justify-center mt-[120px] relative z-[1000]">
       <div className="shadow-md bg-white/80 rounded-lg p-6 w-full max-w-sm">
@@ -51,14 +56,18 @@ export default function ConfirmTime({ params, searchParams }) {
           <p className="text-lg font-bold text-gray-900">{scheduleItem.date}</p>
           <p className="text-lg text-gray-700">Time: {scheduleItem.time}</p>
           <p className="text-lg text-gray-700">
-            Location: {scheduleItem.location}
+            Location: {scheduleItem?.location?.loc}
           </p>
-          <p className={`text-lg ${scheduleItem.available_spots ? 'text-green-600 ': 'text-red-600'  } font-semibold`}>
-            Available Spots: {scheduleItem.available_spots}
+          <p
+            className={`text-lg ${
+              availability ? "text-green-600 " : "text-red-600"
+            } font-semibold`}
+          >
+            Available Spots: {availability}
           </p>
         </div>
 
-        {scheduleItem.available_spots ? (
+        {availability ? (
           <form onSubmit={handleSubmit} className="space-y-4">
             <div>
               <label className="block text-sm font-medium text-gray-700">
@@ -98,14 +107,15 @@ export default function ConfirmTime({ params, searchParams }) {
               </button>
             </div>
           </form>
-        ) : 
-        (
-          <h3 className="mb-5 font-bold">There are no more availble spots for this time.</h3>
+        ) : (
+          <h3 className="mb-5 font-bold">
+            There are no more availble spots for this time.
+          </h3>
         )}
 
         <button
           type="button"
-          onClick={() => router.push('/select-time')}
+          onClick={() => router.push("/select-time")}
           className="w-full text-red-600 px-4 py-4 rounded-md border-red-600 border hover:bg-red-800 hover:text-white transition-colors"
         >
           Select a Different Time
