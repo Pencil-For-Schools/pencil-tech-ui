baca6264e340eb1047f7ebaaa09c6b27b4b6abc0
Lori McCurry <lori@softwareforgood.com>
2024-09-12 11:27:42 -0500

    mock data

---
+++ src/utils/data/sample_400_schedule_item_error_response.js
@@ -0,0 +1,5 @@
+export const sample_schedule_item_error_response = {
+  "message_type": "TEACHER_NOT_SCHEDULED",
+  "message": "Selected time unavailable. Please select a different time.",
+  "email": "email@email.com",
+}
